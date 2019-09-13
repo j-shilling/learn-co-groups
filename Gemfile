76ae7d18395958565e8992e604ce3e184e121fe8
Jake Shilling <shilling.jake@gmail.com>
2019-09-13 16:44:17 -0400

    Adding dotenv to handle secret codes

--- Gemfile
+++ Gemfile
@@ -41,6 +41,8 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger
   # console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  # Shim to load environment variables form .env to ENV in development
+  gem 'dotenv-rails'
 end
 
 group :development do
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@
 # Handles the OAuth2 callback route for logins with learn-co accounts
 class SessionsController < ApplicationController
   # This action gets hit by learn-co after receiving our login request
-  def createp
+  def create
     session[:current_user] = request.env['omniauth.auth']['info']
     redirect_to root_path
   end
