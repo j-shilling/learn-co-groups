31e0924b85ffbec75b4d260a283fb00b2a084e48
Jake Shilling <shilling.jake@gmail.com>
2019-10-04 10:17:08 -0400

    Using before_action to check logged in

--- app/controllers/api/batches_controller.rb
+++ app/controllers/api/batches_controller.rb
@@ -4,22 +4,37 @@ module Api
   class BatchesController < ApplicationController
     include Authenticator
 
+    before_action :check_logged_in
+
     def index
-      if logged_in?
-        api = LearnApi.new current_user['token']
-        batches = api.get('/batches',
-                          page: index_params[:page],
-                          per: index_params[:per])
-        render json: batches
-      else
-        render status: :unauthorized, json: { errors: ['You are not logged in'] }
-      end
+      batches = api.get('/batches',
+                        page: index_params[:page],
+                        per: index_params[:per])
+      render json: batches
+    end
+
+    def show
+      render json: api.get("/batches/#{batch_id}")
     end
 
     private
 
     def index_params
       params.permit(:page, :per)
+    end
+
+    def batch_id
+      params.require(:id)
+    end
+
+    def api
+      LearnApi.new learn_co_token
+    end
+
+    def check_logged_in
+      unless logged_in?
+        render(status: :unauthorized, json: { errors: ['You are not logged in'] })
+      end
     end
   end
 end
