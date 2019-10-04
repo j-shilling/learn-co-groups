0a815692cafb1f2a194c3a8c7f248167dea383b3
Jake Shilling <shilling.jake@gmail.com>
2019-10-04 10:17:35 -0400

    adding route to get users for a batch

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   get '/current_user', to: 'sessions#current_user'
 
   namespace :api, defaults: { format: :json } do
-    resources :batches, only: %i[index show]
+    resources :batches, only: %i[index show] do
+      resources :users, only: %i[index]
+    end
   end
 
   # The front end is going to use react-router. Any routes not matched
