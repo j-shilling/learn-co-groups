2e46befebbf475c95862fa3a76c0ab5867150db8
Jake Shilling <shilling.jake@gmail.com>
2020-01-27 14:44:30 -0500

    Persisting user information upon login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,20 @@
 class SessionsController < ApplicationController
   # This action gets hit by learn-co after receiving our login request
   def create
-    session[:current_user] = request.env['omniauth.auth']['info']
+    user = User.from_omniauth_params(user_params)
+    byebug
+    session[:current_user] = user
     redirect_to root_path
   end
 
   def current_user
+    byebug
     render json: session[:current_user]
-   end
+  end
+
+  private
+
+  def user_params
+    request.env['omniauth.auth']['info']
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  attribute :admin, :boolean
+  attribute :first_name, :string
+  attribute :last_name, :string
+  attribute :token, :string
+
+  def to_s
+    "#{first_name} #{last_name}"
+  end
+
+  def learn_co_attributes=(args)
+    args.each do |k, v|
+      unless k.to_sym == :id
+        send("#{k}=", v) if respond_to? "#{k}="
+      end
+    end
+  end
+
+  def self.from_omniauth_params(args)
+    user = User.find_by_id(args[:id])
+
+    unless user
+      user = User.new
+      user.id = args[:id]
+      user.save
+    end
+
+    user.learn_co_attributes = args
+    user
+  end
+end
