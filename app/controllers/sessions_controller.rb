784b4d7e15d47c66952fa41e861c104bbfc9b28d
Jake Shilling <shilling.jake@gmail.com>
2020-01-27 15:29:01 -0500

    removing forgotten byebugs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,11 @@ class SessionsController < ApplicationController
   # This action gets hit by learn-co after receiving our login request
   def create
     user = User.from_omniauth_params(user_params)
-    byebug
     session[:current_user] = user
     redirect_to root_path
   end
 
   def current_user
-    byebug
     render json: session[:current_user]
   end
 
